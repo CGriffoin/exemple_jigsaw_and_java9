198e4b752845d1d4ab5a4e00c73bba482b74005f
Clément GRIFFOIN <clement.griffoin@gmail.com>
2017-02-05 16:35:31 +0100

    Initialize the step 0

---
+++ src/com.sfeir.printer/module-info.java
@@ -0,0 +1,1 @@
+module com.sfeir.printer { }
--- src/org.fastsocket/module-info.java
+++
@@ -1,5 +0,0 @@
-module org.fastsocket {
-        requires com.socket;
-        provides com.socket.spi.NetworkSocketProvider
-        with org.fastsocket.FastNetworkSocketProvider;
-}
