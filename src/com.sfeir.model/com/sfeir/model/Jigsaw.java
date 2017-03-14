36788602442f3c63e195fe27e0fabd3b870f7ab3
Clément GRIFFOIN <clement.griffoin@gmail.com>
2017-03-14 13:43:48 +0100

    Add models

---
+++ src/com.sfeir.model/com/sfeir/model/Jigsaw.java
@@ -0,0 +1,11 @@
+package com.sfeir.model;
+
+public class Jigsaw {
+    public static String name() {
+        return "Jigsaw";
+    }
+
+    public static String details() {
+        return "Coucou je suis un détail.";
+    }
+}
