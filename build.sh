5c9f9ecd7a8ba0b4cfcdbd51e6af6793ba220db1
Clément GRIFFOIN <clement.griffoin@gmail.com>
2017-03-22 22:17:07 +0100

    add comments and sh script builder.

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sh clean.sh
+sh compileMods.sh
+sh createLibs.sh
+sh createJlink.sh
--- createJlink.sh
+++ createJlink.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 jlink --module-path $JAVA_HOME/jmods:mlib --add-modules printer --output dist/printerapp
+echo "printerapp created."
