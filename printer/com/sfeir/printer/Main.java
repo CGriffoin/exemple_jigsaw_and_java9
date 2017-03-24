a36892dc141ea88cf2bc93f6f0adc0b95ace9e7d
Clément GRIFFOIN <clement.griffoin@gmail.com>
2017-03-24 13:31:56 +0100

    remove useless impots

--- printer/com/sfeir/printer/Main.java
+++ printer/com/sfeir/printer/Main.java
@@ -2,17 +2,16 @@ package printer.com.sfeir.printer;
 
 import model.org.sfeir.model.Jigsaw;
 
-import java.lang.Integer;
-
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello %s %s !%n", Jigsaw.name(), Jigsaw.details());
-        if(args.length == 0) {
+
+        if (args.length == 0) {
             System.out.println("   |--- Add an argument to initialize the counter ! Try 20");
             return;
         }
         Integer starter = Integer.valueOf(args[0]);
-        for(Integer i = starter; i<100;i++) {
+        for (Integer i = starter; i < 100; i++) {
             System.out.println(i);
             try {
                 Thread.sleep(250l);
